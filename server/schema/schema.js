c1b84fd1bbc4c760e026b24da04f9728c4445ef8
Ndubuisi William <princewill835@gmail.com>
2023-09-24 04:21:11 -0700

    created schema adding sample data for testing and Login with registration

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,2 +1,13 @@
 const { GraphQLObjectType, GraphQLID, GraphQLString } = require("graphql");
-        
+const User = new GraphQLObjectType({
+	name: "User",
+	fields: () => ({
+		id: { type: GraphQLID },
+		firstName: { type: GraphQLString },
+		LastName: { type: GraphQLString },
+		city: { type: GraphQLString },
+		phonenumber: { type: GraphQLID },
+		email: { type: GraphQLString },
+		password: { type: GraphQLString },
+	}),
+});
